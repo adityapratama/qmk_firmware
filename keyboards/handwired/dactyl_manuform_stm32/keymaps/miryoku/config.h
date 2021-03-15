56b47ed60360da1fd4d226096f71fa2fbe09a573
Aditya Pratama <aditya.pratama.0010@gmail.com>
2021-03-15 12:24:43 +0700

    Update layer 1 & 7

--- keyboards/handwired/dactyl_manuform_stm32/keymaps/miryoku/config.h
+++ keyboards/handwired/dactyl_manuform_stm32/keymaps/miryoku/config.h
@@ -2,7 +2,7 @@
 
 // https://precondition.github.io/home-row-mods
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 170
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
