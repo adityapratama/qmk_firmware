3e842109540dfb7dd51b6447e35ca6a3da70dc04
Aditya Pratama <aditya.pratama.0010@gmail.com>
2021-03-17 17:56:52 +0700

    Disable Home-row-mods

--- keyboards/handwired/dactyl_manuform_stm32/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform_stm32/keymaps/default/config.h
@@ -1,14 +1,14 @@
-#include "config_common.h"
+// #include "config_common.h"
 
 // https://precondition.github.io/home-row-mods
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+// #define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
+// #define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD_PER_KEY
+// #define TAPPING_FORCE_HOLD_PER_KEY
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
-#define PERMISSIVE_HOLD_PER_KEY
+// #define PERMISSIVE_HOLD_PER_KEY
--- keyboards/handwired/dactyl_manuform_stm32/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_stm32/keymaps/default/keymap.c
@@ -1,23 +1,11 @@
 #include "dactyl_manuformone_stm32.h"
 
-// Left-hand home row mods
-#define ALT_A LALT_T(KC_A)
-#define CTL_O LCTL_T(KC_O)
-#define SUP_E LGUI_T(KC_E)
-#define SHI_U LSFT_T(KC_U)
-
-// Right-hand home row mods
-#define SHI_H RSFT_T(KC_H)
-#define SUP_T RGUI_T(KC_T)
-#define CTL_N RCTL_T(KC_N)
-#define ALT_S RALT_T(KC_S)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, 
 		KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, 
-		MT(MOD_LCTL, KC_ESC), KC_A, KC_O, SUP_E, SHI_U, KC_I, KC_D, SHI_H, SUP_T, KC_N, KC_S, KC_MINS, 
+		MT(MOD_LCTL, KC_ESC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, 
 		KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, 
 		KC_SPC, MT(MOD_LCTL, KC_ESC), MT(MOD_LALT, KC_DOWN), MT(MOD_LGUI, KC_UP), MT(MOD_RGUI, KC_LEFT), MT(MOD_RALT, KC_RGHT), LT(1, KC_ENT), LT(2, KC_BSPC),
 		KC_LGUI, KC_LALT, MT(MOD_LSFT, KC_TAB), MO(1), MO(1), MT(MOD_RSFT, KC_ESC), KC_RALT, KC_RGUI),
@@ -25,8 +13,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_4, KC_5, KC_6, KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_SCLN, kC_4, KC_5, KC_6, KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
@@ -143,101 +131,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 
 };
-
-bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SUP_E:
-            return true;
-        case SHI_U:
-            return true;
-        case SUP_T:
-            return true;
-        case SHI_H:
-            return true;
-        default:
-            return false;
-    }
-}
-
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SUP_E:
-            return true;
-        case SHI_U:
-            return true;
-        case SUP_T:
-            return true;
-        case SHI_H:
-            return true;
-        default:
-            return false;
-    }
-}
-
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SUP_E:
-            return true;
-        case SHI_U:
-            return true;
-        case SUP_T:
-            return true;
-        case SHI_H:
-            return true;
-        default:
-            return false;
-    }
-}
-
-/* const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) { */
-  /* keyevent_t event = record->event; */
-
-  /* switch (id) { */
-
-  /* } */
-	/* return MACRO_NONE; */
-/* } */
-
-/* void matrix_init_user(void) { */
-/* } */
-
-/* void matrix_scan_user(void) { */
-/* } */
-
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-	/* return true; */
-/* } */
-
-/* void led_set_user(uint8_t usb_led) { */
-
-	/* if (usb_led & (1 << USB_LED_NUM_LOCK)) { */
-		
-	/* } else { */
-		
-	/* } */
-
-	/* if (usb_led & (1 << USB_LED_CAPS_LOCK)) { */
-		
-	/* } else { */
-		
-	/* } */
-
-	/* if (usb_led & (1 << USB_LED_SCROLL_LOCK)) { */
-		
-	/* } else { */
-		
-	/* } */
-
-	/* if (usb_led & (1 << USB_LED_COMPOSE)) { */
-		
-	/* } else { */
-		
-	/* } */
-
-	/* if (usb_led & (1 << USB_LED_KANA)) { */
-		
-	/* } else { */
-		
-	/* } */
-
-/* } */
